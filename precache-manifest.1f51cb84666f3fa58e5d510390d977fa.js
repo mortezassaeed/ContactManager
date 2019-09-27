4f4ec802843c5433fa7e45df93b3683d6d63584e
root <mortezassaeed@gmail.com>
2019-09-27 10:44:43 +0330

    Updates

---
+++ precache-manifest.1f51cb84666f3fa58e5d510390d977fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19aeba37c4fabdf5d7286565f3310144",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "6b968b58131bbe4010c0",
+    "url": "/ContactManager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0370bdde8caed7f17837",
+    "url": "/ContactManager/static/css/main.57e35ba2.chunk.css"
+  },
+  {
+    "revision": "6b968b58131bbe4010c0",
+    "url": "/ContactManager/static/js/2.8d2bd456.chunk.js"
+  },
+  {
+    "revision": "0370bdde8caed7f17837",
+    "url": "/ContactManager/static/js/main.f3eb9a67.chunk.js"
+  },
+  {
+    "revision": "90e54fa210d05345aca1",
+    "url": "/ContactManager/static/js/runtime-main.7777eeb1.js"
+  }
+]);
